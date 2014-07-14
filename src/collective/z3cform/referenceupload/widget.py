bb105fc6fd62248e8369a1ce4bb23c253e6c2be6
Radek Jankiewicz <radoslaw.jankiewicz@gmail.com>
2014-07-14 14:24:40 +0200

    remove/store the reference to selected object

--- src/collective/z3cform/referenceupload/widget.py
+++ src/collective/z3cform/referenceupload/widget.py
@@ -1,7 +1,8 @@
 import z3c.form
 from z3c.formwidget.query.widget import QuerySubForm, QueryContext
+from z3c.objpath.interfaces import IObjectPath
 from zope.interface import implements, implementer
-from zope.component import getMultiAdapter
+from zope.component import getMultiAdapter, getUtility
 from plone.formwidget.contenttree.widget import ContentTreeWidget
 
 from .interfaces import IReferenceUploadWidget
@@ -15,24 +16,37 @@ class ReferenceUploadWidget(z3c.form.widget.Widget, ContentTreeWidget):
     klass = u'reference-upload-widget'
     multi_select = False
 
-    def formatted_value(self):
+    def raw_value(self):
+        """Returns physical path of referenced object or None"""
         dm = getMultiAdapter(
             (self.context, self.field), z3c.form.interfaces.IDataManager)
         try:
-            rel = dm.get()
-            if rel:
-                return rel.tag()
+            rel_obj = dm.get()
+            if rel_obj:
+                return '/'.join(rel_obj.getPhysicalPath())
         except TypeError:
             return None
 
+    def formatted_value(self):
+        """Returns url of referenced object or None"""
+        rel_path = self.raw_value()
+        if rel_path:
+            object_path = getUtility(IObjectPath)
+            obj = object_path.resolve(rel_path)
+            return obj.tag()
+        return None
+
     def update(self):
+        """Update widget"""
         self.subform = QuerySubForm(QueryContext(), self.request, self.name)
         self.subform.update()
 
     def renderQueryWidget(self):
+        """Overloaded from z3c.formidget.query.widget.QuerySourceRadioWidget"""
         return ''
 
     def extract(self, default=z3c.form.interfaces.NO_VALUE):
+        """Extract the value of submitted element from the request"""
         option = self.request.get(self.name + '_option')
         if option == u'upload':
             file_upload = self.request.get(self.name + '_file')
@@ -42,6 +56,8 @@ class ReferenceUploadWidget(z3c.form.widget.Widget, ContentTreeWidget):
                     return file_upload
         elif option == u'select':
             return self.request.get(self.name)
+        elif option == u'keep':
+            return self.request.get(self.name + '_raw_value')
         return default
 
 
