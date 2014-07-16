4488f1a6465caaf8f606676cb89b488108a32ca2
Radek Jankiewicz <radoslaw.jankiewicz@gmail.com>
2014-07-16 17:17:44 +0200

    fixed validation and generating file link

--- src/collective/z3cform/referenceupload/interfaces.py
+++ src/collective/z3cform/referenceupload/interfaces.py
@@ -1,5 +1,6 @@
 from z3c.form.interfaces import IWidget
 from z3c.relationfield.interfaces import IRelationChoice
+from zope.schema._bootstrapinterfaces import ValidationError
 
 
 class IReferenceUploadField(IRelationChoice):
@@ -8,3 +9,7 @@ class IReferenceUploadField(IRelationChoice):
 
 class IReferenceUploadWidget(IWidget):
     """ Reference upload widget marker for z3c.form """
+
+
+class ObjectDoesntExist(ValidationError):
+    __doc__ = u"Referenced object doesn't exist"
--- src/collective/z3cform/referenceupload/schema.py
+++ src/collective/z3cform/referenceupload/schema.py
@@ -2,7 +2,8 @@ from zope.interface import implements
 from zope.schema.interfaces import IFromUnicode
 from z3c.relationfield.schema import RelationChoice
 
-from .interfaces import IReferenceUploadField
+from .datamanager import FILE_UPLOAD
+from .interfaces import IReferenceUploadField, ObjectDoesntExist
 
 
 class ReferenceUploadField(RelationChoice):
@@ -11,12 +12,13 @@ class ReferenceUploadField(RelationChoice):
     implements(IReferenceUploadField, IFromUnicode)
 
     def _validate(self, value):
-        return
-        # try:
-        #     obj = self.context.restrictedTraverse(value)
-        #     return super(ReferenceUploadField, self)._validate(obj)
-        # except KeyError:
-        #     return
+        if isinstance(value, FILE_UPLOAD):
+            return
+        try:
+            obj = self.context.restrictedTraverse(value)
+            return super(ReferenceUploadField, self)._validate(obj)
+        except KeyError:
+            raise ObjectDoesntExist(value)
 
     def fromUnicode(self, value):
         if isinstance(value, unicode):
