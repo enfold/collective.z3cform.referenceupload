f769f626e3109ecec72de6888012209d1714bbdb
Joshua LaPlace <jlaplace@enfoldsystems.com>
2014-07-25 19:54:09 +0000

    Add missing supermodel.py and make utility registration conditional.

---
+++ src/collective/z3cform/referenceupload/supermodel.py
@@ -0,0 +1,5 @@
+
+from collective.z3cform.referenceupload.schema import ReferenceUploadField
+from plone.supermodel.exportimport import ChoiceHandler
+
+ReferenceUploadFieldHandler = ChoiceHandler(ReferenceUploadField)
